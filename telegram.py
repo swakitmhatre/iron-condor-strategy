6b74032a382a89512f270b8a558f0af8a1ac7961
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-07-17 11:48:04 +0530

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,12 +1,15 @@
 import requests
+import json
 
 BOT_TOKEN = "7770202577:AAHjZFGJg2Gt3c5S77i__SOCMVqRXd6ofY0"
 CHAT_ID = "1872844861"
 
-def send_telegram_update(message):
+def send_telegram_message(message: str):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    data = {"chat_id": CHAT_ID, "text": message}
+    payload = {"chat_id": CHAT_ID, "text": message}
     try:
-        requests.post(url, data=data, timeout=5)
+        response = requests.post(url, data=payload, timeout=5)
+        if not response.ok:
+            print(f"Telegram failed: {response.status_code}, {response.text}")
     except Exception as e:
         print(f"Telegram error: {e}")
