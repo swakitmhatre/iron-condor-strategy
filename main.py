d3332adc19ed3f1b78f89ee2ba44491c5f29d0ac
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-07-17 11:06:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,4 @@
-# main.py
-
 from strategy import run_strategy
-#from utils import log
-from utils import log_message 
-
 
 if __name__ == "__main__":
-    log_message("Iron Condor strategy running")
-    #log("Main.py started successfully")
-
     run_strategy()
-
-
-'''from strategy import run_strategy
-from utils import log_message
-
-from utils.telegram_bot import send_telegram_message
-import time
-
-logger = setup_logger()
-
-logger.info("Starting Iron Condor strategy")
-
-try:
-    send_telegram_message("🚀 Iron Condor strategy has started.")
-    run_strategy()
-except Exception as e:
-    logger.exception("Exception in main strategy loop")
-    send_telegram_message(f"❌ Strategy crashed with error:\n{e}")'''
