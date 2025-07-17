09f449be439668817f952c06ccc887f6fbc07348
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-07-17 13:18:55 +0530

    Update market_status.py

--- market_status.py
+++ market_status.py
@@ -1,18 +1,7 @@
 # market_status.py
 
 from datetime import datetime, time
-import pytz
 
 def is_market_open():
-    india_tz = pytz.timezone("Asia/Kolkata")
-    now = datetime.now(india_tz)
-    market_open = time(9, 15)
-    market_close = time(15, 30)
-
-    if now.weekday() >= 5:
-        return False, "Market closed (Weekend)"
-    if now.time() < market_open:
-        return False, "Market not yet opened"
-    if now.time() > market_close:
-        return False, "Market closed (Post 3:30 PM)"
-    return True, "Market is open"
+    now = datetime.now().time()
+    return time(9, 15) <= now <= time(15, 30)
