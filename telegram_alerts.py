e4dea26d33417d7c810323dcdf7adf87f3fe4e61
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-07-18 15:00:05 +0530

    Create telegram_alerts.py

---
+++ telegram_alerts.py
@@ -0,0 +1,15 @@
+# telegram_alerts.py
+
+import requests
+from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+
+def send_telegram_message(msg):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": msg
+    }
+    try:
+        requests.post(url, json=payload, timeout=5)
+    except Exception as e:
+        print(f"Telegram error: {e}")
