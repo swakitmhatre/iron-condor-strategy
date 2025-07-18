c9264f0ec72ea75af7dd68bba73c0e90f5fe22e1
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-07-18 12:28:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # main.py
 
 from strategy import run_strategy
-from utils import log_message
 
 if __name__ == "__main__":
-    log_message("Iron Condor strategy running")
-    try:
-        run_strategy()
-    except Exception as e:
-        log_message(f"Unhandled Exception: {e}")
+    import logging
+    from utils import setup_logger
+    setup_logger()
+    logging.info("Iron Condor strategy running")
+    run_strategy()
