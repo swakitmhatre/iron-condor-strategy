00ba44cdcad5cf7630917bc1a01fc7c4c49c5c47
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-07-18 12:55:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # main.py
 
+import logging
 from strategy import run_strategy
 
-if __name__ == "__main__":
-    import logging
-    from utils import setup_logger
-    setup_logger()
-    logging.info("Iron Condor strategy running")
-    run_strategy()
+logging.basicConfig(
+    filename="strategy.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+)
+
+logging.info("Iron Condor strategy running")
+run_strategy()
