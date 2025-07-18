d04995ad8ce32275f286362b00f7ad997d44990c
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-07-18 10:24:39 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,36 +1,10 @@
-
 # utils.py
 
 from datetime import datetime
 
 def log_message(msg):
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    message = f"{timestamp} - {msg}"
-    print(message)
-    with open("strategy.log", "a") as f:
-        f.write(message + "\n")
-
-
-'''from datetime import datetime
-from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
-import requests
-
-def log_message(msg):
     ts = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    final_msg = f"{ts} - {msg}"
-    print(final_msg)
+    text = f"{ts} - {msg}"
+    print(text)
     with open("strategy.log", "a") as f:
-        f.write(final_msg + "\n")
-    send_telegram_message(msg)
-
-def send_telegram_message(text):
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    payload = {
-        "chat_id": TELEGRAM_CHAT_ID,
-        "text": text,
-        "parse_mode": "Markdown"
-    }
-    try:
-        requests.post(url, data=payload)
-    except:
-        pass'''
+        f.write(text + "\n")
