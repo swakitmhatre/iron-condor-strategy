d773cf4a8f99c0ce622b9564bba93f43d8e0b7a8
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-07-18 13:36:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import logging
 from strategy import run_strategy
 
 logging.basicConfig(
-    filename="strategy.log",
     level=logging.INFO,
-    format="%(asctime)s - %(message)s",
+    format='%(asctime)s - %(message)s',
 )
 
-logging.info("Iron Condor strategy running")
-run_strategy()
+if __name__ == "__main__":
+    logging.info("📈 Iron Condor strategy running")
+    try:
+        run_strategy()
+    except Exception as e:
+        logging.exception(f"❌ Strategy failed: {e}")
