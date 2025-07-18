fd14c721b25573a578dd8587262f01a225a2830f
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-07-18 11:02:43 +0530

    Update dhan.py

--- dhan.py
+++ dhan.py
@@ -14,21 +14,21 @@ class Dhan:
         }
 
     def get_nifty_spot(self):
-    url = f"{self.base}/marketfeed/ltp"
-    body = {
-        "instruments": [
-            {
-                "exchangeSegment": "NSE_INDEX",
-                "instrument": "Nifty 50"
-            }
-        ]
-    }
-    resp = requests.post(url, headers=self.headers, json=body, timeout=5)
-    data = resp.json()
-    log_message(f"NIFTY LTP API response: {data}")
-    
-    try:
-        ltp = data["data"]["NSE_INDEX|Nifty 50"]["lastTradedPrice"]
-        return float(ltp)
-    except Exception as e:
-        raise Exception(f"Fetch NIFTY spot failed: {data}")
+        url = f"{self.base}/marketfeed/ltp"
+        body = {
+            "instruments": [
+                {
+                    "exchangeSegment": "NSE_INDEX",
+                    "instrument": "Nifty 50"
+                }
+            ]
+        }
+        resp = requests.post(url, headers=self.headers, json=body, timeout=5)
+        data = resp.json()
+        log_message(f"NIFTY LTP API response: {data}")
+        
+        try:
+            ltp = data["data"]["NSE_INDEX|Nifty 50"]["lastTradedPrice"]
+            return float(ltp)
+        except Exception as e:
+            raise Exception(f"Fetch NIFTY spot failed: {data}")
