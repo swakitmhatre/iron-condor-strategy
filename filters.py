46e2f7cc16225ac331ab8ec92f033c8ffc04bef8
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-07-18 10:25:02 +0530

    Update filters.py

--- filters.py
+++ filters.py
@@ -3,13 +3,13 @@
 from datetime import datetime, time
 from utils import log_message
 
-def time_obj(hr, min):
-    return time(hr, min)
+def time_obj(hr, minute):
+    return time(hr, minute)
 
 def is_market_open():
     now = datetime.now().time()
-    return time(9, 15) <= now <= time(15, 30)
+    return time(9,15) <= now <= time(15,30)
 
 def is_conditions_favorable():
-    # For now always true. You can add IV, news, or trend filters here.
+    # Placeholder
     return True
