80272fbb26c7b0cffab37d1530d50f091593bce2
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-07-24 14:30:18 +0530

    Update iron_condor_final.py

--- iron_condor_final.py
+++ iron_condor_final.py
@@ -64,12 +64,13 @@ def fetch_nifty_spot():
     try:
         res = requests.post(url, headers=HEADERS,json=payload, timeout=0.5)
         #return float(res.json().get("last_price", 22500))
-        print("json---->",res.json())
-        print("spot value org----->",float(res.json()['data']['IDX_I']['13']['last_price']))
+        return float(res.json()['data']['IDX_I']['13']['last_price'])
+        #print("json---->",res.json())
+        #print("spot value org----->",float(res.json()['data']['IDX_I']['13']['last_price']))
         
     except:
-        #return 22500
-        print("spot value----->",22500)
+        return 25000
+        #print("spot value----->",22500)
 
 
 def round_to_strike(price):
