48ae06a1a135ffa4ef3e98f64871dd76226b92da
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-10-01 14:50:21 +0530

    Update iron_condor_flattrade.py

--- iron_condor_flattrade.py
+++ iron_condor_flattrade.py
@@ -149,7 +149,7 @@ def get_live_price(jKey, uid, symbol_token="26000",exch="NSE"):
         }
 
         payload = {
-            "jData": json.dumps(jData),
+            #"jData": jData,
             "jKey": jKey
         }
         print("GetQuotes payload---->",payload)
