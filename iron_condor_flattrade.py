e4e23efc18c5e8dcad6f193fe4d5ef43c921ad8c
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-10-03 15:08:49 +0530

    Update iron_condor_flattrade.py

--- iron_condor_flattrade.py
+++ iron_condor_flattrade.py
@@ -255,7 +255,7 @@ def place_order(jkey, symbol, qty, SIDE):
             "tsym": str(symbol),
             "qty": str(qty),
             "prc": "0",
-            "prd": "IMIS",
+            "prd": "I - MIS",
             "trantype": SIDE,
             "prctyp": "MKT",
             "ret": "DAY"
