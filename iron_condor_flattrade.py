6eab26e81517817e8e55ac67460839a6651b027b
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-10-03 13:01:31 +0530

    Update iron_condor_flattrade.py

--- iron_condor_flattrade.py
+++ iron_condor_flattrade.py
@@ -262,6 +262,7 @@ def get_symbol(expiry, strike, opt_type):
         with open(SYMBOL_FILE) as f:
             for line in f:
                 if f"{UNDERLYING}{expiry}" in line and f"{strike}" in line  and "C" in line or "P" in line:
+                    print("option--->",line)
                     return line.split(",")[4]
     except Exception as e:
         logging.error(f"Symbol lookup failed: {e}")
