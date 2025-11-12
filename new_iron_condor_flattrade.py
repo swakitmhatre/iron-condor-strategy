12fa292404da9db3c46f82ebd5f88274569a7a81
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-11-12 12:28:29 +0530

    Update new_iron_condor_flattrade.py

--- new_iron_condor_flattrade.py
+++ new_iron_condor_flattrade.py
@@ -245,7 +245,7 @@ def get_order_book(JKEY):
 
 def get_entry_price(data,tsym):
 
-    print("data===========>>>>",data.json()))
+    print("data===========>>>>",data.json())
     for order in data:
         
         if order.get("tsym") == tsym:
