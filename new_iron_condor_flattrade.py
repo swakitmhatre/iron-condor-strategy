53bb55141531d1c5cf1b2e5c6bdac657021be60e
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-11-12 12:17:24 +0530

    Update new_iron_condor_flattrade.py

--- new_iron_condor_flattrade.py
+++ new_iron_condor_flattrade.py
@@ -194,7 +194,7 @@ def get_symbol(expiry, strike, opt_type):
         with open(SYMBOL_FILE) as f:
             for line in f:
                 #if f"{UNDERLYING}{expiry}" in line and f"{strike}" in line  and "C" in line or "P" in line:
-                if f"{trading_symbol}" in line and "FINNIFTY" not in line::
+                if f"{trading_symbol}" in line and "FINNIFTY" not in line:
                     print("option--->",line)
                     #return line.split(",")[4]
                     return [line.split(",")[1],line.split(",")[4]]
