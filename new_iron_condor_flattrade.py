3534033d346354ecb1caa8206f4aee02bda36558
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-11-28 13:39:42 +0530

    Update new_iron_condor_flattrade.py

--- new_iron_condor_flattrade.py
+++ new_iron_condor_flattrade.py
@@ -166,11 +166,11 @@ def place_order(JKEY, symbol, qty, SIDE):
         headers = {
             "Content-Type": "application/json"
         }
-        print("order paylod---->",payload)
+        #print("order paylod---->",payload)
         
         #r = requests.post("https://piconnect.flattrade.in/PiConnectTP/PlaceOrder", headers={"Authorization": f"Bearer {token}"}, json=payload)
         r = requests.post("https://piconnect.flattrade.in/PiConnectTP/PlaceOrder", data=payload,headers=headers)
-        print("Order API raw response:", r.text)
+        #print("Order API raw response:", r.text)
         res = r.json()
         logging.info(f"{SIDE} {symbol}: {res}")
         
