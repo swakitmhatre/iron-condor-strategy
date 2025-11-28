bd6792a8c2d417dd81ee3550f43efd8a73513650
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-11-28 14:15:03 +0530

    Update iron_condor_flattrade.py

--- iron_condor_flattrade.py
+++ iron_condor_flattrade.py
@@ -218,7 +218,7 @@ def run_strategy():
     #margin = get_margin(token)
     margin = 100000
     #mtm_target = margin * MTM_PERCENT
-    mtm_target=150
+    mtm_target=50
     lot_size = 75 * LOT_MULTIPLIER
 
     #expiry = datetime.datetime.now().strftime("%y%b%d").upper()
