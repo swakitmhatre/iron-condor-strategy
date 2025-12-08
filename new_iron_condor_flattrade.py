6cd1a97a8e87a176505efed23252c9aebdac1c47
swakitmhatre <33034330+swakitmhatre@users.noreply.github.com>
2025-12-08 13:12:22 +0530

    Update new_iron_condor_flattrade.py

--- new_iron_condor_flattrade.py
+++ new_iron_condor_flattrade.py
@@ -26,8 +26,8 @@ MTM_PERCENT = 0.0025     # 0.25%
 UNDERLYING = "NIFTY"
 
 LOT_SIZE = 75
-TARGET_PROFIT = 15
-STOP_LOSS = -15
+TARGET_PROFIT = 25
+STOP_LOSS = -25
 FALLBACK_AGE = 15          # ✅ if no tick for 30 s, reconnect
 WATCHDOG_INTERVAL = 15     # How often to check tick freshness
 PING_INTERVAL = 10         # How often to send ping for heartbeat
